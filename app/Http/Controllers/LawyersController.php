b268750ebccd462cf73fc9f63825404271f3036d
ashrafhesham-92 <ashraf_treka_6000@hotmail.com>
2018-03-28 17:16:05 +0200

    lawyers

--- app/Http/Controllers/LawyersController.php
+++ app/Http/Controllers/LawyersController.php
@@ -28,7 +28,7 @@ class LawyersController extends Controller
     {
         // return Users::withTrashed()->restore();
         $data['lawyers'] = Users::whereHas('rules', function($q){
-            $q->whereIn('rule_id',[11,12]);
+            $q->where('rule_id',5);
         })->get();
         $data['nationalities'] = Entity_Localizations::where('field','nationality')->where('entity_id',6)->get();
         $data['types']=Rules::whereBetween('id',array('11','12'))->get();
@@ -338,7 +338,7 @@ $myFile= Excel::create('الساده المحامين', function($excel) use($la
         $lawyer->password = bcrypt($password);
         $lawyer->code = 'code-'.Helper::generateRandom(Users::class, 'code', 6);
         $lawyer->save();
-        $lawyer->rules()->attach($request->work_type);
+        $lawyer->rules()->attach([5,$request->work_type]);
         $lawyer_details = new User_Details;
         $lawyer_details->national_id = $request->national_id;
         $lawyer_details->nationality_id = $request->nationality;
@@ -446,7 +446,7 @@ $myFile= Excel::create('الساده المحامين', function($excel) use($la
         // $lawyer->code = 'code-'.Helper::generateRandom(Users::class, 'code', 6);
         $lawyer->save();
         $lawyer->rules()->detach();
-        $lawyer->rules()->attach($request->work_type);
+        $lawyer->rules()->attach([5,$request->work_type]);
         $lawyer_details = User_Details::where('user_id',$id)->first();;
         $lawyer_details->national_id = $request->national_id;
         $lawyer_details->nationality_id = $request->nationality;
