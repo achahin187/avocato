d876f499498979c07272f5cd148fac07038b2463
ashrafhesham-92 <ashraf_treka_6000@hotmail.com>
2018-04-18 12:42:20 +0200

    relation

--- app/Http/Controllers/CasesController.php
+++ app/Http/Controllers/CasesController.php
@@ -173,7 +173,7 @@ class CasesController extends Controller
                  }])->get();
         foreach($lawyers as $detail){
             
-                if(count($detail->user_detail)!=0)
+                if(count($detail->user_detail())!=0)
                 {
                     $value=Helper::localizations('geo_countires','nationality',$detail->user_detail->nationality_id);
               
