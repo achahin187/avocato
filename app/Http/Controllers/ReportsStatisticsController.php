7cb27a3b84eb6ad4c749ae15296225a1be2a1286
basant-mohamed <ahmed.yacoub@outlook.com>
2018-04-24 16:07:12 +0200

    statistics done and fixed list issue

--- app/Http/Controllers/ReportsStatisticsController.php
+++ app/Http/Controllers/ReportsStatisticsController.php
@@ -9,12 +9,14 @@ use Helper;
 use App\Case_;
 use App\Users;
 use App\Tasks;
+use App\Task_Types;
 use App\Installment;
 use App\Courts;
 use App\Cases_Types;
 use App\Geo_Cities;
 use App\Geo_Governorates;
 use App\Geo_Countries;
+use App\Package_Types;
 
 use Illuminate\Http\Request;
 
@@ -101,10 +103,16 @@ class ReportsStatisticsController extends Controller
     public function getData($filters = NULL) 
     {
         /** Filter data */
-        $data['governorates'] = Geo_Governorates::all();
+        $data['governorates']  = Geo_Governorates::all();
         $data['cities'] = Geo_Cities::all();
         $data['nationalities'] = Geo_Countries::all();
         $data['lawyers_'] = Helper::getUsersBasedOnRules([11, 12]);
+        $data['companies_'] = Users::users(9)->get();
+        $data['packages_']  = Package_Types::all();
+        $data['installments_'] = Installment::select('subscription_id')->distinct()->get();
+        $data['courts_']    = Courts::all();
+        $data['tasks_']     = Task_Types::all();
+        $data['caseTypes']  = Cases_Types::all();
 
         /** Countable and percentage data */
         // list data
@@ -126,22 +134,27 @@ class ReportsStatisticsController extends Controller
         $data['count_indcom'] = Users::users(10)->count();
         $data['percentage_indcom'] = Helper::percent($data['count_indcom'], $data['count_clients']);
         
-        /** List data */
-        // cases number & percentage
-        if(isset ( $filters['gov'] ) || isset ( $filters['city'] ) ) { 
-            if(isset($filters['gov'])) { $data['cases'] = Case_::whereIn('geo_governorate_id', $filters['gov']); }
-            if(isset($filters['city'])){ $data['cases'] = Case_::whereIn('geo_city_id', $filters['city']); }
-
-            $data['cases'] = $data['cases']->get();
-        } else {
-            $data['cases'] = Case_::all();
-        }
         $data['count_case'] = Case_::count();
 
         // paid services
         $data['count_paid_services'] = Tasks::where('task_type_id', 3)->where('task_payment_status_id', 2)->count();
         // free services
         $data['count_free_services'] = Tasks::where('task_type_id', 3)->where('task_payment_status_id', 1)->count();
+
+        /** List data */
+        // cases number & percentage
+        if(isset ( $filters['gov'] ) || isset ( $filters['city'] ) ) { 
+            if(isset($filters['gov'])) { 
+                $data['cases'] = Case_::whereIn('geo_governorate_id', $filters['gov'])->select('geo_governorate_id', 'geo_city_id', \DB::raw('count(*) as total'))->groupBy('geo_governorate_id')->groupBy('geo_city_id'); 
+            }
+            if(isset($filters['city'])) { 
+                $data['cases'] = Case_::whereIn('geo_city_id', $filters['city'])->select('geo_governorate_id', 'geo_city_id', \DB::raw('count(*) as total'))->groupBy('geo_governorate_id')->groupBy('geo_city_id'); 
+            }
+
+            $data['cases'] = $data['cases']->get();
+        } else {
+            $data['cases'] = Case_::select('geo_governorate_id', 'geo_city_id', \DB::raw('count(*) as total'))->groupBy('geo_governorate_id')->groupBy('geo_city_id')->get();
+        }
 
         // Lawyers
         if ( isset ( $filters['sector'] ) || isset ( $filters['level'] ) || isset ( $filters['nationality'] ) || isset ( $filters['title'] ) || isset ( $filters['type'] )  ) {
@@ -178,23 +191,146 @@ class ReportsStatisticsController extends Controller
         }
         
         // Companies
-        $data['companies'] = Users::users(9)->get();
+        if ( isset( $filters['company'] ) ||isset( $filters['packages'] ) ||isset( $filters['activate'] ) ) { 
+            $packages = $filters['packages'];
+            $data['companies'] = Users::users(9);
+
+            if ( isset( $filters['company'] ) ) {
+                $data['companies'] = $data['companies']->where('users.id', $filters['company']);
+            }
+            if ( isset( $filters['packages'] ) ) {
+                $data['companies'] = $data['companies']->whereHas('subscription', function($q) use($packages) {
+                    $q->whereIn('package_type_id', $packages);
+                });
+            }
+            if ( isset( $filters['activate'] ) ) {
+                if($filters['activate'] == 1) {
+                    $data['companies'] = $data['companies']->get();
+                } else if($filters['activate'] == 2) {
+                    $data['companies'] = $data['companies']->where('is_active', 1)->get();
+                } else if($filters['activate'] == 3) {
+                    $data['companies'] = $data['companies']->where('is_active', 0)->get();
+                }
+            }
+        } else {
+            $data['companies'] = Users::users(9)->get();
+        }
         
         // Installments
-        $data['installments'] = Installment::all();
+        if ( isset( $filters['code'] ) || isset( $filters['startDate'] ) || isset( $filters['activate1'] ) ) {
+            // dd([$filters['code']  , $filters['startDate'] , $filters['activate1']]);
+            $data['installments'] = new Installment;
 
+            if ( isset($filters['code']) ) {
+                $data['installments'] = $data['installments']->where('subscription_id', $filters['code']);
+            }
+
+            if ( isset($filters['startDate']) ) {
+                $startDate = date('Y-m-d', strtotime($filters['startDate']));
+                $data['installments'] = $data['installments']->whereHas('subscription', function($q) use($startDate) {
+                    $q->where('start_date', '>=', $startDate);
+                });
+            }
+
+            if ( isset($filters['activate1']) ) {
+                if($filters['activate1'] == 1) { 
+                    $data['installments'] = $data['installments']->get();
+                }
+                if($filters['activate1'] == 2) {
+                    $data['installments'] = $data['installments']->where('is_paid', 1)->get();
+                }
+                if($filters['activate1'] == 3) {
+                    $data['installments'] = $data['installments']->where('is_paid', 0)->get();
+                }
+            }
+
+        } else {
+            $data['installments'] = Installment::all();
+        }
+        
         // Urgents
-        $data['urgents'] = Helper::getUsersBasedOnRules([7, 8, 9, 10]);
+        if ( isset( $filters['userType'] ) ) {
+            $data['urgents'] = Helper::getUsersBasedOnRules($filters['userType']);
+        } else {
+            $data['urgents'] = Helper::getUsersBasedOnRules([7, 8, 9, 10]);
+        }
+       
 
         // Courts
-        $data['courts'] = Courts::all();
+        if ( isset( $filters['cities'] ) || isset( $filters['govs'] ) || isset( $filters['courts'] ) ) {
+
+            $data['courts'] = new Courts;
+
+            if( isset($filters['cities']) ) {
+                $data['courts'] = $data['courts']->whereIn('city_id', $filters['cities']);
+            }
+
+            if( isset($filters['govs']) ) {
+                $govs = $filters['govs'];
+                $data['courts'] = $data['courts']->whereHas('city', function($q) use($govs) {
+                    $q->whereHas('governorate', function($m) use($govs) {
+                       $m->whereIn('id', $govs); 
+                    });
+                });
+            }
+
+            if( isset($filters['courts']) ) {
+                $data['courts'] = $data['courts']->whereIn('id', $filters['courts']);
+            }
+
+            $data['courts'] = $data['courts']->get();
+        } else {
+            $data['courts'] = Courts::select('city_id', 'name', \DB::raw('count(*) as total') )->groupBy('city_id')->groupBy('name')->get();
+        }
 
         // Tasks
-        $data['tasks'] = Tasks::all();
+        if ( isset( $filters['taskType'] ) || isset( $filters['dateFrom'] ) || isset( $filters['dateTo'] ) ) {
+            
+            $data['tasks'] = new Task_Types;
+            
+            if ( isset( $filters['taskType'] ) ) {
+                $data['tasks'] = $data['tasks']->whereIn('id', $filters['taskType']);
+            }
 
+            if ( isset( $filters['dateFrom'] ) ) {
+                $dateFrom = $filters['dateFrom'];
+                $data['tasks'] = $data['tasks']->whereHas('tasks', function($q) use($dateFrom) {
+                    $q->where('start_datetime', '>=', $dateFrom);
+                });
+            }
+
+            if ( isset( $filters['dateTo'] ) ) {
+                $dateTo = $filters['dateTo'];
+                $data['tasks'] = $data['tasks']->whereHas('tasks', function($q) use($dateTo) {
+                    $q->where('end_datetime', '<=', $dateTo);
+                });
+            }
+
+            $data['tasks'] = $data['tasks']->get();
+        } else {
+            $data['tasks'] = Task_Types::all();
+        }
         // Case type
-        $data['casesTypes'] = Cases_Types::all();
+        if ( isset( $filters['cities1'] ) ||isset( $filters['govs1'] ) || isset( $filters['caseType'] ) ) {
 
+            // dd([$filters['cities1'], $filters['govs1'], $filters['caseType']]);
+            $data['cases1'] = new Case_;
+
+            if ( isset( $filters['cities1'] ) ) {
+                $data['cases1'] = $data['cases1']->whereIn('geo_city_id', $filters['cities1'])->select(['case_type_id', 'geo_city_id', 'geo_governorate_id'])->groupBy('case_type_id')->groupBy('geo_city_id')->groupBy('geo_governorate_id');
+            }
+            if ( isset( $filters['govs1'] ) ) {
+                $data['cases1'] = $data['cases1']->whereIn('geo_governorate_id', $filters['govs1'])->select(['case_type_id', 'geo_city_id', 'geo_governorate_id'])->groupBy('case_type_id')->groupBy('geo_city_id')->groupBy('geo_governorate_id');
+            }
+            if ( isset( $filters['caseType'] ) ) {
+                $data['cases1'] = $data['cases1']->whereIn('case_type_id', $filters['caseType'])->select(['case_type_id', 'geo_city_id', 'geo_governorate_id'])->groupBy('case_type_id')->groupBy('geo_city_id')->groupBy('geo_governorate_id');
+            }
+
+            $data['cases1'] = $data['cases1']->get();
+        } else {
+            $data['cases1'] = Case_::select(['case_type_id', 'geo_city_id', 'geo_governorate_id'])->groupBy('case_type_id')->groupBy('geo_city_id')->groupBy('geo_governorate_id')->get();
+        }
+        
         return $data;
     }
 
@@ -206,6 +342,36 @@ class ReportsStatisticsController extends Controller
             // lawyers tab
             if( $request->workSector || $request->level || $request->nationality || $request->workTitle || $request->workSectorType ) {
                 $data = $this->getData(['sector'=>$request->workSector, 'level'=>$request->level, 'nationality'=>$request->nationality, 'title'=>$request->workTitle, 'type'=>$request->workSectorType]);
+            }
+
+            // company tab
+            if ( $request->company || $request->packages || $request->activate ) {
+                $data = $this->getData(['company'=>$request->company, 'packages'=>$request->packages, 'activate'=>$request->activate]);
+            }
+
+            // installments
+            if ( $request->code || $request->startDate || $request->activate1 ) {
+                $data = $this->getData(['code'=> $request->code, 'startDate'=> $request->startDate, 'activate1'=>$request->activate1]);
+            }
+            
+            // Urgents
+            if ( $request->userType ) {
+                $data = $this->getData(['userType'=>$request->userType]);
+            }
+
+            // Courts
+            if ( $request->cities || $request->govs || $request->courts ) {
+                $data = $this->getData(['cities'=>$request->cities, 'govs'=>$request->govs, 'courts'=>$request->courts]);
+            }
+
+            // Tasks
+            if ( $request->taskType || $request->dateFrom || $request->dateTo ) {
+                $data = $this->getData(['taskType'=>$request->taskType, 'dateFrom'=>$request->dateFrom, 'dateTo'=>$request->dateTo]);
+            }
+
+            // Cases Types
+            if ( $request->cities1 || $request->govs1 || $request->caseType ) {
+                $data = $this->getData(['cities1'=>$request->cities1, 'govs1'=>$request->govs1, 'caseType'=>$request->caseType]);
             }
 
         } else {
