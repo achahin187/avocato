014b684d1f8f051828deff667641df90fe8b8470
basant-mohamed <ahmed.yacoub@outlook.com>
2018-04-26 13:00:02 +0200

    fix success message

--- resources/views/consultations_classification.blade.php
+++ resources/views/consultations_classification.blade.php
@@ -443,6 +443,12 @@
         }
       });
 
+      // hide alert message after 4 seconds => 4000 ms
+      window.setTimeout(function() {
+            $(".alert").fadeTo(500, 0).slideUp(500, function(){
+                $(this).remove(); 
+            });
+        }, 4000);
     });
   </script>
 
