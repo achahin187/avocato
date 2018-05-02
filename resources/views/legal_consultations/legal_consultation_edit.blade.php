ceee7499c73a48a4e5c608198bf6af5574a91496
ashrafhesham-92 <ashraf_treka_6000@hotmail.com>
2018-05-02 14:54:25 +0200

    consultations test

--- resources/views/legal_consultations/legal_consultation_edit.blade.php
+++ resources/views/legal_consultations/legal_consultation_edit.blade.php
@@ -89,7 +89,7 @@
                       </button>
                     </div>
                     <div class="col-md-2 col-xs-6">
-                      <button class="master-btn undefined btn-block color--white bgcolor--fadebrown bradius--small bshadow--0" type=""><i class="fa fa-times"></i><span>الغاء</span>
+                      <button class="master-btn undefined btn-block color--white bgcolor--fadebrown bradius--small bshadow--0" type="button" onclick="{{route('legal_consultations')}}"><i class="fa fa-times"></i><span>الغاء</span>
                       </button>
                     </div>
                     <div class="clearfix"></div><br>
