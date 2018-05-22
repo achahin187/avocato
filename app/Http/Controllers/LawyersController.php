f318d1f6701bd4b7d12f70a5ee462663ca1d6a4a
SaraMagdyAbdelhamed <sara.magdy1191995@gmail.com>
2018-05-22 14:47:37 +0200

    rate fix

--- app/Http/Controllers/LawyersController.php
+++ app/Http/Controllers/LawyersController.php
@@ -319,7 +319,11 @@ class LawyersController extends Controller
     $data['types'] = Entity_Localizations::where('entity_id',9)->where('field','name')->get();
     $data['statuses'] = Entity_Localizations::where('entity_id',4)->where('field','name')->get();
     $data['expenses'] = Expenses::where('lawyer_id',$id)->get();
-    $data['rates_user'] = $data['lawyer']->rate()->with('rules')->get();
+    if($data['lawyer']->rate() != null)
+    {
+      $data['rates_user'] = $data['lawyer']->rate()->with('rules')->get();
+    }
+    $data['rates_user'] = null;
     // dd($data['rates_user']);
     $data['rates'] = Entity_Localizations::where('entity_id',10)->where('field','name')->get();
     return view('lawyers.lawyers_show',$data);
