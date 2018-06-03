8f56c43e3b3b22d3e701008217e7270a94ad0067
SaraMagdyAbdelhamed <sara.magdy1191995@gmail.com>
2018-06-03 12:09:23 +0200

    k

--- resources/views/lawyers/lawyers.blade.php
+++ resources/views/lawyers/lawyers.blade.php
@@ -240,7 +240,7 @@ $(document).on('confirmation', '#two', function () {
             </div>
           </div>
           <div class="filter__btns"><a class="master-btn bgcolor--main color--white bradius--small" href="#filterModal_sponsors"><i class="fa fa-filter"></i>filters</a></div>
-          <div class="bottomActions__btns"><a class="noti-all master-btn bradius--small padding--small bgcolor--fadeorange color--white" href="#">ارسال تنبية</a><a class="excel-btn master-btn bradius--small padding--small bgcolor--fadeblue color--white" href="#">استخراج اكسيل</a><a class="master-btn bradius--small padding--small bgcolor--fadebrown color--white btn-warning-cancel-all" href="#">حذف المحدد</a>
+          <div class="bottomActions__btns"><a class="noti-all master-btn bradius--small padding--small bgcolor--fadeorange color--white" >ارسال تنبية</a><a class="excel-btn master-btn bradius--small padding--small bgcolor--fadeblue color--white" >استخراج اكسيل</a><a class="master-btn bradius--small padding--small bgcolor--fadebrown color--white btn-warning-cancel-all" >حذف المحدد</a>
           </div>
           <table class="table-1" id="dataTableTriggerId_001">
             <thead>
