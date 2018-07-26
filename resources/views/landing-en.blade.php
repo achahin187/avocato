b66124d5213f5fa20921cf117bcebcd9bbd4820b
ashrafhesham-92 <sara.magdy@pentavalue.com>
2018-07-26 17:08:06 +0200

    L

--- resources/views/landing-en.blade.php
+++ resources/views/landing-en.blade.php
@@ -91,7 +91,7 @@ Avocato app The leading only society for legal services</h4>
                       <li class="tab fancyTab">
                         <div class="arrow-down">
                           <div class="arrow-down-inner"></div>
-                        </div><a id="tab2" href="#tabBody2" role="tab" aria-controls="tabBody2" aria-selected="true" data-toggle="tab" tabindex="0"><span class="fa fa-briefcase"></span><span class="hidden-xs">Office - Company</span></a>
+                        </div><a id="tab2" href="#tabBody2" role="tab" aria-controls="tabBody2" aria-selected="true" data-toggle="tab" tabindex="0"><span class="fa fa-black-tie"></span><span class="hidden-xs">Office - Company</span></a>
                         <div class="whiteBlock"></div>
                       </li>
                     </ul>
