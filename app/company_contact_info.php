61c1355e7640791b137b9cae20172e57e0fa7788
unknown <abdullah.abdelaziz89@gmail.com>
2018-10-09 14:19:42 +0200

    Contact_us form

---
+++ app/company_contact_info.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class company_contact_info extends Model
+{
+    //
+    public $timestamps=false;
+    protected $table='company_contact_info';
+
+}
---
+++ app/company_social_media.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class company_social_media extends Model
+{
+    //
+    public $timestamps=false;
+    protected $table='company_social_media';
+}
