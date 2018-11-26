2d76169de7a94d2bad459abf7b688e1aef672b40
saramagdy <sara.magdy@pentavalue.com>
2018-11-26 12:48:56 +0200

    m

--- app/Geo_Cities.php
+++ app/Geo_Cities.php
@@ -8,8 +8,8 @@ class Geo_Cities extends Model
 {
     protected $primaryKey = 'id';
     protected $table = 'geo_cities';
-    protected $fillable = ['governorate_id', 'name'];
-    protected $hidden = ['id', 'created_at', 'updated_at','country_id'];
+    protected $fillable = ['governorate_id', 'name','country_id'];
+    protected $hidden = ['id', 'created_at', 'updated_at'];
     public $timestamps = true;
 
         public function governorate()
