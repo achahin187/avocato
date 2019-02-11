194744114e20df473327f2039d62a4370ce96497
ashrafhesham-92 <sara.magdy@pentavalue.com>
2019-02-11 16:39:35 +0200

    image

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,9 +21,67 @@ class AppServiceProvider extends ServiceProvider
     {
         view()->composer('*', function ($view) 
         {
-            dd(\Session::get('country'));
+            // dd(\Session::get('country'));
             //this code will be executed when the view is composed, so session will be available
-            $view->with('country', \Session::get('country') );    
+            $view->with('country', \Session::get('country') );  
+            $notes = Notifications::where('country_id', \Session::get('country'))->where(function ($query) {
+                $query->where('is_read', '=', 0)->where('is_push', '=', 0)->orWhere(function ($query){
+                    $query->where('is_read',1)->whereDate('created_at', DB::raw('CURDATE()'));
+                });
+    
+            })->orderBy('created_at','desc')->get();
+               
+            foreach($notes as $note){
+                if($note->notification_type_id ==2){
+    
+                $note['url']='task_emergency_view';
+                }
+                if($note->notification_type_id ==3){
+                    $user = Users::find($note->item_id);
+                    if($user->getClient()==8){
+                        $note['url']='ind.show';
+                    }
+                    if($user->getClient()==9){
+                        $note['url']='companies.show';
+                    }
+                    if($user->getClient()==10){
+                        $note['url']='ind.com.show';
+                    }
+                }
+                if($note->notification_type_id ==4){
+    
+                $note['url']='complains.edit';
+                }
+                if($note->notification_type_id ==5 and $note->entity_id==11 ){
+    
+                $note['url']='services_show';
+                }
+                if($note->notification_type_id ==5 and $note->entity_id==14 ){
+    
+                $note['url']='case_view';
+                }
+                if($note->notification_type_id ==6){
+    
+                $note['url']='lawyers_show';
+                }
+                if($note->notification_type_id ==7){
+    
+                $note['url']='legal_consultation_view';
+                }
+                if($note->notification_type_id ==16){
+    
+                    $note['url']='lawyers_show';
+                    }
+    
+            }
+            $counter = 0;
+            foreach($notes as $note){
+                if($note->is_read==0)
+                    $counter++;
+            }
+    
+            \View::share('counter', $counter);
+            \View::share('notes', $notes);  
         }); 
         Schema::defaultStringLength(191);
         Sheet::macro('Bolding', function (Sheet $sheet, string $cellRange) {
@@ -39,64 +97,7 @@ class AppServiceProvider extends ServiceProvider
         });
 
 
-        $notes = Notifications::where('country_id', \Session::get('country'))->where(function ($query) {
-            $query->where('is_read', '=', 0)->where('is_push', '=', 0)->orWhere(function ($query){
-                $query->where('is_read',1)->whereDate('created_at', DB::raw('CURDATE()'));
-            });
-
-        })->orderBy('created_at','desc')->get();
-           
-        foreach($notes as $note){
-            if($note->notification_type_id ==2){
-
-            $note['url']='task_emergency_view';
-            }
-            if($note->notification_type_id ==3){
-                $user = Users::find($note->item_id);
-                if($user->getClient()==8){
-                    $note['url']='ind.show';
-                }
-                if($user->getClient()==9){
-                    $note['url']='companies.show';
-                }
-                if($user->getClient()==10){
-                    $note['url']='ind.com.show';
-                }
-            }
-            if($note->notification_type_id ==4){
-
-            $note['url']='complains.edit';
-            }
-            if($note->notification_type_id ==5 and $note->entity_id==11 ){
-
-            $note['url']='services_show';
-            }
-            if($note->notification_type_id ==5 and $note->entity_id==14 ){
-
-            $note['url']='case_view';
-            }
-            if($note->notification_type_id ==6){
-
-            $note['url']='lawyers_show';
-            }
-            if($note->notification_type_id ==7){
-
-            $note['url']='legal_consultation_view';
-            }
-            if($note->notification_type_id ==16){
-
-                $note['url']='lawyers_show';
-                }
-
-        }
-        $counter = 0;
-        foreach($notes as $note){
-            if($note->is_read==0)
-                $counter++;
-        }
-
-        \View::share('counter', $counter);
-        \View::share('notes', $notes);
+       
     }
 
     /**
