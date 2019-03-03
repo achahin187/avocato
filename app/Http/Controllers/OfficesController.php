1fb572fe6b7ec4759930f060bb549cee1d49e85a
ashrafhesham-92 <sara.magdy@pentavalue.com>
2019-03-03 19:51:51 +0200

    notifications

--- app/Http/Controllers/OfficesController.php
+++ app/Http/Controllers/OfficesController.php
@@ -181,7 +181,7 @@ class OfficesController extends Controller
       $validator = Validator::make($request->all(), [
       'office_name' => 'required',
       'office_email' => 'required|email',
-      'office_mobile' => 'required|min:13|max:12|unique:users,mobile,,,deleted_at,NULL',
+      'office_mobile' => 'required|regex:/^\+?[^a-zA-Z]{5,}$/|min:13|max:12|unique:users,mobile,,,deleted_at,NULL',
       'office_city' => 'required',
       'rep_name' => 'required',
       'rep_birthdate' => 'required|date',
@@ -451,7 +451,7 @@ class OfficesController extends Controller
     $validator = Validator::make($request->all(), [
         'office_name' => 'required',
         'office_email' => 'required|email',
-        'office_mobile' => 'required||min:13|max:13',
+        'office_mobile' => 'required|regex:/^\+?[^a-zA-Z]{5,}$/|min:13|max:13',
         'office_city' => 'required',
         'rep_name' => 'required',
         'rep_birthdate' => 'required|date',
