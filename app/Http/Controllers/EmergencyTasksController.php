ddac4f59f8ed0fcf6de546f8b25371514a2d3258
saramagdy <sara.magdy@pentavalue.com>
2019-07-09 17:41:51 +0200

    m

--- app/Http/Controllers/EmergencyTasksController.php
+++ app/Http/Controllers/EmergencyTasksController.php
@@ -37,7 +37,7 @@ class EmergencyTasksController extends Controller
       $data['charges'] = Task_Charges::where('task_id', $id)->get();
       $data['types'] = Entity_Localizations::where('entity_id', 9)->where('field', 'name')->get();
       $data['statuses'] = Entity_Localizations::where('entity_id', 4)->where('field', 'name')->get();
-      $data['reports'] = Case_Techinical_Report::where('item_id', $id)->where('technical_report_type_id', 1)->get();
+      $data['reports'] = Case_Techinical_Report::where('item_id', $data['task']->id)->where('technical_report_type_id', 1)->get();
  dd($data['reports']);
     	return view('tasks.emergency_view',$data);
     }
