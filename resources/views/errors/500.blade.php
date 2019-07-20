473e50af36d62603393fe75f1046ffece065d9f8
saramagdy <sara.magdy@pentavalue.com>
2019-07-21 01:30:31 +0200

    consultation

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,1 +1,19 @@
-<h1>This page doesn't exist</h1>
+@extends('layout.app')
+@section('content')
+
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="cardwrap bgcolor--white bradius--noborder   bshadow--1 padding--small margin--small-top-bottom">
+                    <div class="col-sm-8 col-md-offset-2 col-xs-12 col-xs-offset-0"><img class="logo img-responsive" src="{{ asset('img/logo/logo__light.png') }}" alt="جسر الأمان"></div>
+                    
+                    <div class="clearfix"></div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-12">
+                   <h1>500 NoT Found</h1>
+                </div>
+              </div>
+
+@endsection
--- resources/views/errors/502.blade.php
+++ resources/views/errors/502.blade.php
@@ -1,1 +1,19 @@
-<h1 style="color:red; font-size:100%">This is error handler page</h1>
+@extends('layout.app')
+@section('content')
+
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="cardwrap bgcolor--white bradius--noborder   bshadow--1 padding--small margin--small-top-bottom">
+                    <div class="col-sm-8 col-md-offset-2 col-xs-12 col-xs-offset-0"><img class="logo img-responsive" src="{{ asset('img/logo/logo__light.png') }}" alt="جسر الأمان"></div>
+                    
+                    <div class="clearfix"></div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-12">
+                   <h1>502 NoT Found</h1>
+                </div>
+              </div>
+
+@endsection
