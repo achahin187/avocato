0f53ad0677e6ccbfb35ed236a98d0c82c4e186dc
saramagdy <sara.magdy@pentavalue.com>
2019-07-23 15:47:52 +0200

    rate

--- resources/views/lawyers/lawyers_show.blade.php
+++ resources/views/lawyers/lawyers_show.blade.php
@@ -469,7 +469,7 @@
                                 <td><span class="cellcontent">
 
                                 
-                                <a href="#edit_evaluation_modal" class= "action-btn bgcolor--fadegreen color--white" id="edit_rating" data-item-id="{{$rate->pivot->id}}">
+                                <a href="#edit_evaluation_modal{{$rate->pivot->id}}" class= "action-btn bgcolor--fadegreen color--white" id="edit_rating" data-item-id="{{$rate->pivot->id}}">
 
                                  <i class = "fa fa-pencil"></i></a>
                                 <a  href="#"  class= "btn-warning-cancel-rate action-btn bgcolor--fadebrown color--white ">
@@ -478,7 +478,7 @@
 
                                 
                               </tr>
-                              <div class="remodal" data-remodal-id="{{route('notes_edit',$rate->pivot->id)}}" role="dialog" aria-labelledby="modal2Title" aria-describedby="modal2Desc">
+                              <div class="remodal" data-remodal-id="edit_evaluation_modal{{$rate->pivot->id}}" role="dialog" aria-labelledby="modal2Title" aria-describedby="modal2Desc">
                                   <button class="remodal-close" data-remodal-action="close" aria-label="Close"></button>
                                   <form role="form" action="{{route('notes_edit_admin',$lawyer->id)}}" method="post" accept-charset="utf-8">
                                      {{csrf_field()}}
