799667b392f416fe356bd87905820baf8a2caa6a
saramagdy <sara.magdy@pentavalue.com>
2019-08-15 13:57:09 +0200

    M

--- resources/views/tasks/tasks_emergency.blade.php
+++ resources/views/tasks/tasks_emergency.blade.php
@@ -482,7 +482,7 @@ var client_long;
           //    $('.remodal-bg').hide();
           //    // $('.modal-backdrop').remove();
           // });
-          $('[data-remodal-id=add_task_service]').cancel();
+          $('[data-remodal-id=add_task_service]').remodal('close');;
             //  window.location.reload();
              
               // alert(data);
