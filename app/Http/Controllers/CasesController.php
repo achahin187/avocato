a8be4fdf4de67ed0b50d7dfbd8a53152f0d5d89d
saramagdy <sara.magdy@pentavalue.com>
2019-08-15 14:35:54 +0200

    M

--- app/Http/Controllers/CasesController.php
+++ app/Http/Controllers/CasesController.php
@@ -855,7 +855,7 @@ class CasesController extends Controller
         } else {
             $type = $_GET['type'];
             Excel::store((new CasesExport(null, $type)), $filepath . $filename);
-            return response()->json($PathForJson . $filename);
+            return response()->json(storage_path($filename));
         }
     }
 
