e5cfea6af86ba6d4bd6d58eea0fa19844b5e2847
saramagdy <sara.magdy@pentavalue.com>
2019-08-15 13:21:47 +0200

    M

--- resources/views/clients/individuals/individuals_show.blade.php
+++ resources/views/clients/individuals/individuals_show.blade.php
@@ -56,7 +56,200 @@
         <div class="clearfix"></div>
       </div>
     </div>
- 
+    <div class="col-md-12">
+      <div class="tabs--wrapper">
+        <div class="clearfix"></div>
+        <ul class="tabs">
+          <li>معلومات التعاقد</li>
+          <li>القضايا</li>
+          <li>الخدمات </li>
+          <li>الطلبات الطارئة</li>
+          <li>فهرس التوكيلات</li>
+        </ul>
+        <ul class="tab__content">
+         
+            <li class="tab__content_item">
+              <div class="cardwrap bgcolor--white bradius--noborder   bshadow--1 padding--small margin--small-top-bottom">
+                <table class="table-1">
+                  <thead>
+                    <tr class="bgcolor--gray_mm color--gray_d">
+                      <th><span class="cellcontent">نوع القضية</span></th>
+                      <th><span class="cellcontent">المحكمة</span></th>
+                      <th><span class="cellcontent">الدائرة</span></th>
+                      <th><span class="cellcontent">رقم الدعوى</span></th>
+                      <th><span class="cellcontent">السنة</span></th>
+                      <th><span class="cellcontent">تاريخ قيد الدعوى</span></th>
+                      <th><span class="cellcontent">رقم الملف بالمكتب</span></th>
+                      <th><span class="cellcontent">رقم التوكيل</span></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @if (isset($cases) && !empty($cases))
+                        @foreach ($cases as $case)
+                        <tr>
+                          <td><span class="cellcontent">{{ ($case->cases->case_types) ? $case->cases->case_types->name : 'لا يوجد' }}</span></td>
+                          <td><span class="cellcontent">{{ ($case->cases->courts) ? $case->cases->courts->name : 'لا يوجد' }}</span></td>
+                          <td><span class="cellcontent">{{ ($case->cases->region) ? $case->cases->region : 'لا يوجد' }}</span></td>
+                          <td><span class="cellcontent">{{ ($case->cases->claim_number) ? $case->cases->claim_number : 'لا يوجد' }}</span></td>
+                          <td><span class="cellcontent">{{ ($case->cases->claim_year) ? $case->cases->claim_year : 'لا يوجد' }}</span></td>
+                          <td><span class="cellcontent">{{ $case->cases ? ($case->cases->claim_date ? $case->cases->claim_date->format('d/m/Y') : 'لا يوجد') : 'لا يوجد' }}</span></td>
+                          <td><span class="cellcontent">{{ ($case->cases->office_file_number) ? $case->cases->office_file_number : 'لا يوجد' }}</span></td>
+                          <td><span class="cellcontent">{{ ($case->attorney_number) ? $case->attorney_number : 'لا يوجد' }}</span></td>
+                        </tr>
+                        @endforeach
+                    @endif
+                  </tbody>
+                </table>
+              </div>
+            </li>
+            <li class="tab__content_item">
+              <div class="cardwrap bgcolor--white bradius--noborder   bshadow--1 padding--small margin--small-top-bottom">
+                <table class="table-1">
+                  <thead>
+                    <tr class="bgcolor--gray_mm color--gray_d">
+                      <th><span class="cellcontent">نوع الخدمة</span></th>
+                      <th><span class="cellcontent">القائم بالاجراء</span></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @if (isset($services) && !empty($services))
+                        @foreach ($services as $service)
+                        <tr>
+                          <td><span class="cellcontent">{{ ($service->task_payment_status_id) ? Helper::localizations('task_payment_statuses', 'name', $service->task_payment_status_id) : 'لا يوجد' }}</span></td>
+                          <td><span class="cellcontent">{{ ($service->assigned_lawyer_id) ? Helper::getUserDetails($service->assigned_lawyer_id)->full_name : 'لا يوجد' }}</span></td>
+                        </tr>
+                        @endforeach
+                    @endif
+                  </tbody>
+                </table>
+              </div>
+            </li>
+            <li class="tab__content_item">
+              <div class="cardwrap bgcolor--white bradius--noborder   bshadow--1 padding--small margin--small-top-bottom">
+                <table class="table-1">
+                  <thead>
+                    <tr class="bgcolor--gray_mm color--gray_d">
+                      <th><span class="cellcontent">عنوان الطلب</span></th>
+                      <th><span class="cellcontent">تفاصيل الطلب</span></th>
+                      <th><span class="cellcontent">الحالة</span></th>
+                      <th><span class="cellcontent"> التاريخ</span></th>
+                      <th><span class="cellcontent">الوقت</span></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @if (isset($urgents) && !empty($urgents))
+                        @foreach ($urgents as $urgent)
+                        <tr>
+                          <td><span class="cellcontent">{{ ($urgent->task_address) ? $urgent->task_address : 'لا يوجد' }}</span></td>
+                          <td><span class="cellcontent">{{ ($urgent->description)  ? $urgent->description  : 'لا يوجد' }}</span></td>
+                          <td><span class="cellcontent"><i class = "fa {{ ($urgent->task_status_id == 2) ? 'color--fadegreen fa-check' : 'fa-times' }}"></i></span></td>
+                          <td><span class="cellcontent">{{ ($urgent->start_datetime) ? $urgent->start_datetime->format('d-m-Y') : 'لا يوجد' }}</span></td>
+                          <td><span class="cellcontent">{{ ($urgent->start_datetime) ? $urgent->start_datetime->format('h:m')   : 'لا يوجد' }}</span></td>
+                        </tr>
+                        @endforeach
+                    @endif
+                  </tbody>
+                </table>
+              </div>
+            </li>
+            <li class="tab__content_item">
+              <div class="cardwrap bgcolor--white bradius--noborder   bshadow--1 padding--small margin--small-top-bottom">
+                <table class="table-1">
+                  <thead>
+                    <tr class="bgcolor--gray_mm color--gray_d">
+                      <th><span class="cellcontent">م</span></th>
+                      <th><span class="cellcontent">رقم التوكيل</span></th>
+                      <th><span class="cellcontent">مكتب التوثيق</span></th>
+                      <th><span class="cellcontent">نوعه</span></th>
+                      <th><span class="cellcontent"> التاريخ</span></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($procurations as $procuration)
+                    <tr>
+                      <td><span class="cellcontent">{{$loop->iteration}}</span></td>
+                      <td><span class="cellcontent">{{$procuration->procuration_number}}</span></td>
+                      <td><span class="cellcontent">{{$procuration->office}}</span></td>
+                      <td><span class="cellcontent">{{$procuration->procuration_type}}</span></td>
+                      <td><span class="cellcontent">{{$procuration->issue_date}}</span></td>
+                    </tr>
+                    @endforeach
+                   
+                  </tbody>
+                </table>
+                <div class="col-md-2 col-sm-6 col-xs-12"><a class="master-btn color--white bgcolor--main bradius--small bshadow--0 btn-block" href="#popupModal_2"><i class="fa fa-plus"></i><span>اضافة</span></a>
+                  <div class="remodal-bg"></div>
+                  <div class="remodal" data-remodal-id="popupModal_2" role="dialog" aria-labelledby="modal1Title" aria-describedby="modal2Desc">
+                     <form role="form" action="{{route('procuration.store')}}" method="post" accept-charset="utf-8">
+                          {{csrf_field()}}
+                          <!-- id   client_id   procuration_number  procuration_type    issue_date  office  photo   created_at  updated_at  created_by  updated_by -->
+                    <button class="remodal-close" data-remodal-action="close" aria-label="Close"></button>
+                    <div>
+                      <div class="row">
+                       <input  type="hidden" name="client_id" value="{{$user->id}}">
+                             <input  type="hidden" name="route_name" value="ind.show">
+                        <div class="col-xs-12">
+                          <h3>اضافة</h3>
+                        </div>
+                        <div class="col-xs-6">
+                          <div class="master_field">
+                            <label class="master_label mandatory" for="ID_No">رقم التوكيل</label>
+                            <input class="master_input" type="number" placeholder="رقم التوكيل" id="ID_No" name="procuration_number">
+                          </div><span class="master_message color--fadegreen">  @if ($errors->has('procuration_number'))
+                                      {{ $errors->first('procuration_number')}}
+                                    @endif</span>
+                        </div>
+                        <div class="col-xs-6">
+                          <div class="master_field">
+                            <label class="master_label mandatory" for="ID_No">مكتب التوثيق</label>
+                            <input class="master_input" type="text" placeholder="مكتب التوثيق" id="ID_No" name="office">
+                          </div><span class="master_message color--fadegreen">  @if ($errors->has('office'))
+                                      {{ $errors->first('office')}}
+                                    @endif</span>
+                        </div>
+                        <div class="col-xs-6">
+                          <div class="master_field">
+                            <label class="master_label mandatory" for="ID_No">نوعه</label>
+                            <input class="master_input" type="text" placeholder="نوعه" id="ID_No" name="procuration_type">
+                          </div><span class="master_message color--fadegreen">  @if ($errors->has('procuration_type'))
+                                      {{ $errors->first('procuration_type')}}
+                                    @endif</span>
+                        </div>
+                        <div class="col-xs-6">
+                          <div class="master_field">
+                            <label class="master_label mandatory" for="ID_No">التاريخ</label>
+                            <input class=" datepicker master_input" type="text" placeholder="التاريخ" id="issue_date" name="issue_date">
+                          </div><span class="master_message color--fadegreen">  @if ($errors->has('issue_date'))
+                                      {{ $errors->first('issue_date')}}
+                                    @endif</span>
+                        </div>
+                        <div class="col-xs-12">
+                          <div class="master_field">
+                            <label class="master_label mandatory" for="ID_No-11">صورة التوكيل </label>
+                            <div class="file-upload">
+                              <div class="file-select">
+                                <div class="file-select-name" id="noFile">صورة التوكيل </div>
+                                 <input name="image" class="chooseFile" type="file" name="chooseFile" id="lawyer_img">
+                            </div>
+                            </div><span class="master_message color--fadegreen">  @if ($errors->has('photo'))
+                                      {{ $errors->first('photo')}}
+                                    @endif</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div><br>
+                    <button class="remodal-cancel" data-remodal-action="cancel">الغاء</button>
+                    <button class="remodal-confirm"  type="submit">اضافة</button>
+                  </form>
+                  </div>
+                </div>
+                <div class="clearfix"></div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
 
     <script>
       $(document).ready(function() {
