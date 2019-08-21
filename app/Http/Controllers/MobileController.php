30bae66f2136eee557435393b83289eeed940bea
saramagdy <sara.magdy@pentavalue.com>
2019-08-21 14:30:33 +0200

    M

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -102,7 +102,7 @@ class MobileController extends Controller
        
             if(array_key_exists('search',$request->all()))
             {
-                dd($request->all());
+                // dd($request->all());
                 
                 $filter->where('name','like','%'.$request->search.'%')->orwhere('full_name','like','%'.$request->search.'%')->orwhere('code','like','%'.$request->search.'%');
             }
