c935cd8b88650767ca7b54534c7cf68ceec50573
saramagdy <sara.magdy@pentavalue.com>
2019-08-26 16:53:19 +0200

    offices

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -273,11 +273,11 @@ class ServicesController extends Controller
 
         if($request->filled('search'))
         {
-            $data['services'] = $data['services']->where('name','like','%'.$request->search.'%')->orwhere(function($query) use ($request){
+            $data['services'] = $data['services']->distinct()->where('name','like','%'.$request->search.'%')->orwhere(function($query) use ($request){
                 $query->whereHas('client',function($q) use ($request){
-                    $q->where(function($q) use ($request){
+                    
                         $q->where('full_name','like','%'.$request->search.'%')->orwhere('code','like','%'.$request->search.'%');
-                    });
+                   
                 });
             }); 
         }
