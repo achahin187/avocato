ac42e08e909605eee0548b8113b10ed8b8d84d66
saramagdy <sara.magdy@pentavalue.com>
2019-10-12 17:35:22 +0200

    M

--- app/Http/Controllers/LegalConsultationsController.php
+++ app/Http/Controllers/LegalConsultationsController.php
@@ -253,6 +253,7 @@ class LegalConsultationsController extends Controller
             }
 
         }
+        dd($consultation);
         return view('legal_consultations.legal_consultation_view')->with('consultation', $consultation);
     }
 
