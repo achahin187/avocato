f2a6385a21decef00c07fbb670cc4dcb40a5c643
amira samy <amira.samy@pentavalue.com>
2019-12-04 13:14:09 +0200

    amira

--- resources/views/reports_statistics.blade.php
+++ resources/views/reports_statistics.blade.php
@@ -3,7 +3,7 @@
 
 <div class="row">
   <div class="col-lg-12">
-    <div class="cover-inside-container margin--small-top-bottom bradius--small bshadow--1" style="background:  url( '../img/covers/dummy2.jpg ' ) no-repeat center center; background-size:cover;">
+    <div class="cover-inside-container margin--small-top-bottom bradius--small bshadow--1" style="background:  url( '{{asset('img/covers/dummy2.jpg')}}' ) no-repeat center center; background-size:cover;">
       <div class="row">
         <div class="col-xs-12">
           <div class="text-xs-center">
