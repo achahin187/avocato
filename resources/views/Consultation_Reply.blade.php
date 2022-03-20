4eaf91ec91aaea2cda2fefc8e294ccf5c23904db
achahin187 <achahin187@gnail.com>
2022-03-20 13:16:36 +0200

    add new api create consultation

--- resources/views/Consultation_Reply.blade.php
+++ resources/views/Consultation_Reply.blade.php
@@ -5,8 +5,9 @@ Welcome to Avocato,
 <br>
 your Consultation Answer is : <h2>  {{$data['consultation_answer']}} </h2>
 <br>
-<h3>    shorturl.at/moIZ6 /تحميل التطبيق اندرويد
-    shorturl.at/fqDEتحميل التطبيق  للأيفون/ 8
+<h3>  
+      shorturl.at/moIZ6 /تحميل التطبيق اندرويد
+      shorturl.at/fqDE8 /تحميل التطبيق  للأيفون  
 </h3>
 <br>
 Have a nice day
