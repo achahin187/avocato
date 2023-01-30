94ce815acdadee4c4dc2b19f04961dfb430cb850
achahin187 <achahin187@gmail.com>
2023-01-30 16:23:21 +0200

    fixing bugs

--- resources/views/cases/case_add.blade.php
+++ resources/views/cases/case_add.blade.php
@@ -40,7 +40,7 @@
                       <div class="col-md-3 col-sm-6 col-xs-12">
                             <div class="master_field">
                           <label class="master_label mandatory" for="client_code_0">كود العميل</label>                       
-                           <select class="master_input select2 required"  id="client_code_0" name="client_code[0]" style="width:100%;" onchange="set_client_data(this.value,0,{{$data['clients']}})" required>
+                           <select class="master_input select2 required"  id="client_code_0" name="client_code[0]" style="width:100%;"  required>
                             <option value="-1" selected disabled hidden>إختر كود العميل</option>
                             @foreach($data['clients'] as $client)
                               <option id="comcode" value="{{ $client->id }}" data-id="{{ $client->id}}">{{ $client->code .' - '. $client->name}}</option>
